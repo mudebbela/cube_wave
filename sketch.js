a3dae32506d864967150bd1af6de35f9eb613f8a
mudebbela <mudebbela@yahoo.com>
2018-01-11 16:55:11 -0500

    Workable, with scale factor added

--- sketch.js
+++ sketch.js
@@ -1,32 +1,42 @@
 let angle = 0;
-let w = 20;
-let offset =0;
+let w = 25;
+let ma;
+let maxD;
 
 function setup(){
   createCanvas(400,400, WEBGL);
+  ma = atan(1/sqrt(2));
+  maxD = dist(0,0, 200,200);
 }
 
 function draw(){
-  background(100);
-  translate(0, 25, -50);
-  //translate(width/4, height/2);
+  background(175);
+  ortho(width, -width, -height, height,-100, 1000);
+  translate(0,-50,-250);
+  rotateX(-PI/4)
+  rotateY(ma);
   rectMode(CENTER);
 
-  //rotateX(angle);
-  for(let x = 0; x<width; x+= w){
-    push();
-    let newAngle = map(offset, 0, 20, -1 , 1 );
-    let h =map(sin(angle+ newAngle), -1, 1,20,100);
-    console.log(newAngle);
+  let offset = 0;
+  for (let z = 0; z<height; z+=w){
+    for(let x = 0; x< width; x+=w){
+      push();
+      let d = dist(x, z, width/2, height/2)
+      let offset = map(d, 0, maxD, -PI, PI);
+      let a =  angle +offset
+      let h = map(sin(a),-1, 1, 150, 350);
+      let scaleFactor = map(mouseX, 0 , 400, -0.99, 1);
 
+      scale(1+ scaleFactor);
+      fill(255);
+      translate(x - width / 2,0 , z- height/2);
+      normalMaterial();
+      box(w, h , w);
+      pop();
+    }
 
-    fill(255);
-    translate(x- width/2,0,0)
-    //rect(x,0,9,h);
-    box(w, h, w);
-    offset++;
-    pop()
+    offset +=0.1;
+
   }
-angle+=0.1;
-offset = 0;
+  angle-= 0.1;
 }
